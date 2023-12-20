cd32fa1ec527de7a408858757c27c77c6b6553cb
nguyendaihongquan <quanedungso@gmail.com>
2023-12-20 16:59:31 +0700

    up base project

---
+++ lib/router/app_router.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:vote_app/router/router_name.dart';
+import 'package:vote_app/screen/home_screen.dart';
+
+class AppRouter {
+  AppRouter._instance();
+  static final AppRouter instance = AppRouter._instance();
+  Route? onGenerateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case RouteName.home:
+        return MaterialPageRoute(
+            builder: (context) => const HomeScreen(),
+            settings: const RouteSettings(name: RouteName.home));
+    }
+    return null;
+  }
+}
---
+++ lib/router/router_name.dart
@@ -0,0 +1,4 @@
+class RouteName {
+  static const login = '/x';
+  static const home = '/';
+}
