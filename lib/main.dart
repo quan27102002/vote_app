4f0d4dad23128ce99e762fe7013acd8b29602725
nguyendaihongquan <quanedungso@gmail.com>
2023-12-21 15:15:21 +0700

    new branch

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.deepPurple,
+          seedColor: Colors.white,
         ),
         useMaterial3: true,
       ),
