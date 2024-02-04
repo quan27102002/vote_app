7276bbf4f8e5c7b0c0a0b1434230d4e8da8a7cac
ongquang2290 <quangdang229@gmail.com>
2024-02-04 23:02:37 +0700

    modify BE code

---
+++ backend/VPRatingAppBE/VPBE.Service/Interfaces/ITokenService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VPBE.Service.Interfaces
+{
+    public class ITokenService
+    {
+    }
+}
