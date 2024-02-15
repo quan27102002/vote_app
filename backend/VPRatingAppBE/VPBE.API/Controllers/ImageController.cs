6183a2903b0b2d76a96a6f55855a10eca27c86db
ongquang2290 <quangdang229@gmail.com>
2024-02-15 12:19:41 +0700

    add upload/get images

--- backend/VPRatingAppBE/VPBE.API/Controllers/ImageController.cs
+++ backend/VPRatingAppBE/VPBE.API/Controllers/ImageController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 using VPBE.API.Controllers.Base;
+using VPBE.Domain.Attributes;
+using VPBE.Domain.Dtos;
+using VPBE.Domain.Entities;
 using VPBE.Domain.Utils;
 using VPBE.Service.Interfaces;
 
@@ -16,6 +20,8 @@ namespace VPBE.API.Controllers
             this._hostEnvironment = hostEnvironment;
         }
         [HttpPost("bulkupload")]
+        [Role(new UserRole[] { UserRole.Admin })]
+        [SwaggerResponse(200, Type = typeof(APIResponseDto<bool>))]
         public async Task<IActionResult> BulkUpload(IFormFileCollection formFiles)
         {
             int successCount = 0;
@@ -26,6 +32,12 @@ namespace VPBE.API.Controllers
                 {
                     logger.Debug($"Create directory {path}");
                     Directory.CreateDirectory(path);
+                }
+                DirectoryInfo directoryInfo = new DirectoryInfo(path);
+                FileInfo[] fileInfos = directoryInfo.GetFiles();
+                foreach (FileInfo fileInfo in fileInfos)
+                {
+                    fileInfo.Delete();
                 }
                 foreach (var item in formFiles)
                 {
@@ -40,7 +52,11 @@ namespace VPBE.API.Controllers
                         successCount++;
                     }
                 }
-                return Ok();
+                return Ok(new CustomResponse
+                {
+                    Result = true,
+                    Message = $"{successCount}/{formFiles.Count} files successfully uploaded."
+                });
             }
             catch (Exception ex)
             {
@@ -52,6 +68,45 @@ namespace VPBE.API.Controllers
         private string GetImagePath()
         {
             return _hostEnvironment.WebRootPath + "\\assets\\images";
+        }
+
+        [HttpGet("get")]
+        [Role(new UserRole[] { UserRole.Admin })]
+        [SwaggerResponse(200, Type = typeof(APIResponseDto<List<string>>))]
+        public async Task<IActionResult> GetImages()
+        {
+            var host = $"{Request.Scheme}://{Request.Host}{Request.PathBase}";
+            var imageUrls = new List<string>();
+            try
+            {
+                var path = GetImagePath();
+                if (Directory.Exists(path))
+                {
+                    DirectoryInfo directoryInfo = new DirectoryInfo(path);
+                    FileInfo[] fileInfos = directoryInfo.GetFiles();
+                    var listFileName = fileInfos.Select(a => a.Name);
+                    foreach (var item in listFileName)
+                    {
+                        string imagepath = path + "\\" + item;
+                        if (System.IO.File.Exists(imagepath))
+                        {
+                            string url = host + "/assets/images/" + item;
+                            imageUrls.Add(url);
+                        }
+                    }
+                }
+
+                return Ok(new CustomResponse
+                {
+                    Result = imageUrls,
+                    Message = ""
+                });
+            }
+            catch (Exception ex)
+            {
+                logger.Error($"Error getting images. Message: {ex.Message}", ex);
+                throw;
+            }
         }
     }
 }
